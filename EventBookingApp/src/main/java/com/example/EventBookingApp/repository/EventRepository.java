6cd9983d6d007e32ad6b4346c964af1711f1d2d7
FedericoIafolla <iafollafederico@gmail.com>
2024-11-08 16:03:05 +0100

    Correzione delle repositories

--- EventBookingApp/src/main/java/com/example/EventBookingApp/repository/EventRepository.java
+++ EventBookingApp/src/main/java/com/example/EventBookingApp/repository/EventRepository.java
@@ -3,4 +3,8 @@ package com.example.EventBookingApp.repository;
 import com.example.EventBookingApp.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventRepository extends JpaRepository<Event, Long> {}
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findAll();
+}
--- EventBookingApp/src/main/java/com/example/EventBookingApp/repository/UserRepository.java
+++ EventBookingApp/src/main/java/com/example/EventBookingApp/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.EventBookingApp.repository;
 
-import com.example.EventBookingApp.model.User;
+import com.example.EventBookingApp.model.App;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
+public interface UserRepository extends JpaRepository<App, Long> {
+    Optional<App> findByUsername(String username);
 }
